35c48b8a4b2ff32b64486dbf2c2c5498d4b5b47d
Ivan <alexandrovis@gmail.com>
2021-02-04 12:48:06 +0300

    третий вариант решения

--- HW_4/main.js
+++ HW_4/main.js
@@ -4,6 +4,7 @@ do {
 }
 while (!(resNumber >= 0));
 
+//1-й вариант. Нахожу его плохим, потому что я возвращаю три объекта, в зависимости от ситуации, но нем не менее на ретурн я пишу три разных объекта
 function conversion(item) {
   if (item > 999) {
     console.log("Большое число");
@@ -36,6 +37,7 @@ function conversion(item) {
 conversion(resNumber);
 console.log(result);
 
+//2-й варинат, тут возвращаем один объект, но если цифр меньше чем три, то пустые ячейки объекта принимают undefined
 function conversion2(item) {
   if (item > 999) {
     console.log("Большое число");
@@ -52,3 +54,29 @@ function conversion2(item) {
 }
 conversion2(resNumber);
 console.log(result);
+
+//3-й Вариант решения. Здесь я сделал шаблон объекта и принял число в функию, функция вызывает др функцию, которая разделяет число в массив и возвращает массив, а потом первая функция расставляет значения с массива по местам из шаблона объекта. мне кажется, это самый верный вариант, дайте , пожалуйста обратную свзять по этому варианту. 
+
+function Mine(hundreds = "", tens = "", units = "") {
+  this.units = units;
+  this.tens = tens;
+  this.hundreds = hundreds;
+}
+
+function conver(arg) {
+  if (arg <= 999) {
+    slit(arg);
+    MyObject = new Mine(arr[2], arr[1], arr[0]);
+  }
+  else {
+    console.log("Число вышло за диапазон");
+    return MyObject = new Mine();
+  }
+}
+
+function slit(arg) {
+  return arr = String(arg).split('').reverse();;
+};
+
+conver(resNumber);
+console.log(MyObject);
