67a553be877f4295aaa1a46fb70331601a634d63
Ivan <alexandrovis@gmail.com>
2021-02-08 18:04:24 +0300

    ДЗ по 5-му уроку

---
+++ HW_5/data.js
@@ -0,0 +1,163 @@
+var figures = [
+  {
+    name: "White Chess Pawn 1",
+    title: "&#9817",
+    position: "71"
+  },
+  {
+    name: "White Chess Pawn 2",
+    title: "&#9817",
+    position: "72"
+  },
+  {
+    name: "White Chess Pawn 3",
+    title: "&#9817",
+    position: "73"
+  },
+  {
+    name: "White Chess Pawn 4",
+    title: "&#9817",
+    position: "74"
+  },
+  {
+    name: "White Chess Pawn 5",
+    title: "&#9817",
+    position: "75"
+  },
+  {
+    name: "White Chess Pawn 6",
+    title: "&#9817",
+    position: "76"
+  },
+  {
+    name: "White Chess Pawn 7",
+    title: "&#9817",
+    position: "77"
+  },
+  {
+    name: "White Chess Pawn 8",
+    title: "&#9817",
+    position: "78"
+  },
+  {
+    name: "White Chess King",
+    title: "&#9812",
+    position: "85"
+  },
+  {
+    name: "White Chess Queen",
+    title: "&#9813",
+    position: "84"
+  },
+  {
+    name: "White Chess Bishop 1",
+    title: "&#9815;",
+    position: "83"
+  },
+  {
+    name: "White Chess Bishop 2",
+    title: "&#9815;",
+    position: "86"
+  },
+  {
+    name: "White Chess Rook 1",
+    title: "&#9814",
+    position: "81"
+  },
+  {
+    name: "White Chess Rook 2",
+    title: "&#9814",
+    position: "88"
+  },
+  {
+    name: "White Chess Knight 1",
+    title: "&#9816",
+    position: "82"
+  },
+  {
+    name: "White Chess Knight 2",
+    title: "&#9816",
+    position: "87"
+  },
+  // 
+  {
+    name: "White Chess Pawn 1",
+    title: "&#9823",
+    position: "21"
+  },
+  {
+    name: "Black Chess Pawn 2",
+    title: "&#9823",
+    position: "22"
+  },
+  {
+    name: "Black Chess Pawn 3",
+    title: "&#9823",
+    position: "23"
+  },
+  {
+    name: "Black Chess Pawn 4",
+    title: "&#9823",
+    position: "24"
+  },
+  {
+    name: "Black Chess Pawn 5",
+    title: "&#9823",
+    position: "25"
+  },
+  {
+    name: "Black Chess Pawn 6",
+    title: "&#9823",
+    position: "26"
+  },
+  {
+    name: "Black Chess Pawn 7",
+    title: "&#9823",
+    position: "27"
+  },
+  {
+    name: "Black Chess Pawn 8",
+    title: "&#9823",
+    position: "28"
+  },
+  {
+    name: "Black Chess King",
+    title: "&#9818",
+    position: "15"
+  },
+  {
+    name: "Black Chess Queen",
+    title: "&#9819",
+    position: "14"
+  },
+  {
+    name: "Black Chess Bishop 1",
+    title: "&#9821;",
+    position: "13"
+  },
+  {
+    name: "Black Chess Bishop 2",
+    title: "&#9821;",
+    position: "16"
+  },
+  {
+    name: "Black Chess Rook 1",
+    title: "&#9820",
+    position: "11"
+  },
+  {
+    name: "Black Chess Rook 2",
+    title: "&#9820",
+    position: "18"
+  },
+  {
+    name: "Black Chess Knight 1",
+    title: "&#9822",
+    position: "12"
+  },
+  {
+    name: "Black Chess Knight 2",
+    title: "&#9822",
+    position: "17"
+  },
+]
